f501cb25b64f3eefac0eb3cfef6ea6a15bc9f6ea
Connor Escajeda <escajedacj@hendrix.edu>
2021-10-11 19:40:21 -0500

    Added seeded data, starting step 5 with updating pages

---
+++ RazorPageSong/RazorPageSong/Models/SeedData.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPageSong.Data;
+using System;
+using System.Linq;
+
+namespace RazorPageSong.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPageSongContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<RazorPageSongContext>>()))
+            {
+                // Look for any movies.
+                Console.WriteLine(context.Song.Any());
+                if (context.Song.Any())
+                {   
+                    Console.WriteLine("test");
+                    return;   // DB has been seeded
+                }
+
+                context.Song.AddRange(
+                    new Song
+                    {
+                    
+                        Title = "Perfect",
+                        Artist = "Logic",
+                        Album = "Bobby Tarantino",
+                        Genre = "Rap",
+                        Plays = 100000
+                    },
+
+                    new Song
+                    {
+                       
+                        Title = "Super",
+                        Artist = "Cordae",
+                        Album = "Super",
+                        Genre = "Rap",
+                        Plays = 100000
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
